3987b4f7cf0148805e275b86bf566924784309d0
Fardhan Erfandyar <fardh.er@gmail.com>
2024-02-15 13:30:01 +0700

    app bar

--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,16 +7,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Viva Sports",
-          style: TextStyle(
-              color: Colors.black, fontSize: 19, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        "Viva Sports",
+        style: TextStyle(
+            color: Colors.black, fontSize: 19, fontWeight: FontWeight.bold),
       ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      actions: [
+        GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              alignment: Alignment.center,
+              width: 37,
+              child: SvgPicture.asset(
+                'assets/icons/inbox-svgrepo-com.svg',
+                height: 20,
+                width: 18,
+              ),
+              decoration: BoxDecoration(
+                  color: const Color(0xffF7F8f8),
+                  borderRadius: BorderRadius.circular(10)),
+            ))
+      ],
     );
   }
 }
